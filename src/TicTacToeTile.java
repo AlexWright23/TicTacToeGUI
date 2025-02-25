a4802983d1c555d98ab29e559ad67a289e5f124b
Alexander Wright <Alexwright45177@gmail.com>
2025-02-24 23:04:11 -0500

    Update TicTacToeTile.java

--- src/TicTacToeTile.java
+++ src/TicTacToeTile.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+//jbutton
 
-public class TicTacToeTile {
+import javax.swing.JButton;
+
+public class TicTacToeTile extends JButton {
+    private int row, col;
+    private String symbol = "";  // stores x and o
+
+    public TicTacToeTile(int row, int col) {
+        super();
+        this.row = row;
+        this.col = col;
+        this.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 40));
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public boolean isEmpty() {
+        return symbol.equals("");
+    }
+
+    public void setSymbol(String symbol) {
+        if (isEmpty()) {
+            this.symbol = symbol;
+            this.setText(symbol);
+        }
+    }
+
+    public void resetTile() {
+        this.symbol = "";
+        this.setText("");
+    }
 }
